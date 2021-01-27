17cbb5823eac0d894daa214c821bf3cf632d0c54
Alican ERTOP <alicanertop@hotmail.com>
2021-01-27 03:15:44 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27._01._2021
+{
+
+    static class Exam
+    {
+        public static void QuestionOne()
+        {
+            Console.WriteLine($"Test");
+        }
+
+        public static void QuestionTwo()
+        {
+
+        }
+
+        public static void QuestionThree()
+        {
+
+        }
+
+        public static void QuestionFour()
+        {
+
+        }
+
+    }
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Exam.QuestionOne();
+
+            Console.ReadKey();
+        }
+    }
+}
